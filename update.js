c93acc85cc56fd93c440d41c8d601c2b6f645bc2
Joyee Cheung <joyeec9h3@gmail.com>
2018-01-27 18:12:09 +0800

    initial commit

---
+++ update.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+if (!process.argv[2]) {
+  console.error('Usage: update.js path/to/node/project');
+  process.exit(1);
+}
+
+const pluginRules = require('@joyeecheung/eslint-plugin-node-core').rules;
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+const NODE_DIR = process.argv[2];
+const file = fs.readFileSync(
+  path.join(NODE_DIR, '.eslintrc.yaml'), 'utf8');
+
+const config = yaml.safeLoad(file);
+const configRules = Object.keys(config.rules);
+for (const key of configRules) {
+  if (pluginRules[key]) {
+    const value = config.rules[key];
+    delete config.rules[key];
+    config.rules[`@joyeecheung/eslint-plugin-node-core/${key}`] = value;
+  }
+}
+fs.writeFileSync('eslintrc.json', JSON.stringify(config, null, 2));
